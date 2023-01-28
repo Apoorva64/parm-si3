2937dfac81ca03d76d243710441ce33cd0c39566
Ozeliurs <ozeliurs@gmail.com>
2023-01-29 00:49:43 +0100

    docs: add missing docs

--- code_asm/src/assembler.py
+++ code_asm/src/assembler.py
@@ -1,16 +1,22 @@
 from pathlib import Path
+from typing import Dict
 
 from pyparsing import Word, hexnums, Optional, alphas, alphanums
 
 from assembler_utils import assemble_register, assemble_imm5, assemble_imm8, assemble_imm3, assemble_imm7_offset, \
-    assemble_imm8_offset, signed_imm8, signed_imm11
+    assemble_imm8_offset, signed_imm8, signed_imm11, to_hex
 
+"""
+key of dict is the 
+instruction name
+number of registers
+whether there is an immediate value,
+whether there is SP and
+whether there is a label
+"""
+asm_map: Dict = {
 
-def to_hex(binary: str) -> str:
-    return f"{int(binary, 2):04x}"
-
-
-asm_map = {
+    # data processing
     'lsls2100': lambda rd, rm, imm5: "000" +
                                      "00" +
                                      assemble_imm5(imm5) +
@@ -110,11 +116,14 @@ asm_map = {
     'add0110': lambda sp, offset: "1011" + "0000" + "0" + assemble_imm7_offset(offset),
     'sub0110': lambda sp, offset: "1011" + "0000" + "1" + assemble_imm7_offset(offset),
 
+    # Load/store with register offset
     'str1110': lambda rt, rn, offset: "1001" + "0" + assemble_register(rt) + assemble_imm8_offset(offset),
     'ldr1110': lambda rt, rn, offset: "1001" + "1" + assemble_register(rt) + assemble_imm8_offset(offset),
 
+    # Unconditional branch
     'b0001': lambda offset: "11100" + signed_imm11(offset),
 
+    # Conditional branch
     'bEQ0001': lambda offset: "1101" + "0000" + signed_imm8(offset),
     'bNE0001': lambda offset: "1101" + "0001" + signed_imm8(offset),
     'bCS0001': lambda offset: "1101" + "0010" + signed_imm8(offset),
@@ -137,25 +146,46 @@ asm_map = {
 
 class Assembler:
     def __init__(self):
+        # instructions are just letters
         self.instruction = Word(alphas).setResultsName('instruction')
         # registers starts with r and followed by a number
         self.register = Word("r", alphanums).setResultsName("register", listAllMatches=True)
+        # immediate starts with a #
         self.immediate = Word("#", hexnums).setResultsName("immediate")
+        # sp matches with sp
         self.sp = Word("sp").setResultsName("sp", listAllMatches=True)
+
         self.operand = (self.register | self.immediate | self.sp)
         self.operand_list = self.operand + Optional("," + self.operand) + Optional("," + self.operand)
+
+        # Match comments
         self.comment = Word("@").setResultsName("comment")
+        # Match Labels
         self.label = Word(".", alphanums).setResultsName("label")
+
+        # Configure the global parser
         self._parser = (self.instruction + Optional(self.operand_list) + Optional(self.comment) + Optional(
             self.label)) | self.comment | self.label
+        # A dict to store the labels addresses
         self.labels = {}
+        # The program counter
         self.program_counter = 0
 
     def parse(self, line):
+        """
+        Parse a single line
+        :param line: the line to parse
+        :return: the parsed line
+        """
         line = line.replace("[", "").replace("]", "")
         return self._parser.parseString(line)
 
     def assemble(self, line):
+        """
+        Assemble a single line
+        :param line: the line to assemble
+        :return: the assembled line
+        """
         parse = self.parse(line).as_dict()
 
         # if the line is a comment or a label, return empty string
@@ -184,12 +214,17 @@ class Assembler:
         return to_hex(asm_map[instruction](*args))
 
     def build_labels_dict(self, filename):
+        """
+        Build a dict of labels and their addresses
+        :param filename: the file to parse
+        """
         pointer = 0
         for line in Path(filename).read_text().splitlines():
             if line.strip() == "":
                 continue
             line = self.parse(line)
             if 'label' in line and 'instruction' not in line:
+                # Preparse the file to get the labels addresses
                 self.labels[line.get('label')] = pointer
                 continue
             if 'comment' in line:
@@ -197,6 +232,12 @@ class Assembler:
             pointer += 2
 
     def assemble_file(self, filename):
+        """
+        Assemble a file
+        :param filename: the file to assemble
+        :return: a string containing the assembled file
+        """
+        # Preparse the file to get the labels addresses
         self.build_labels_dict(filename)
         output = []
         for line in Path(filename).read_text().splitlines():
--- code_asm/src/assembler_utils.py
+++ code_asm/src/assembler_utils.py
@@ -1,18 +1,33 @@
-def assemble_register(register):
+def assemble_register(register: str) -> str:
+    """
+    Assemble a register string into a 3-bit binary string.
+    :param register: the register to assemble
+    :return: the register as a 3-bit binary string
+    """
     register = int(register[1:])
     if register < 0 or register > 7:
         raise ValueError(f"Invalid register {register}")
     return f"{register:03b}"
 
 
-def assemble_imm8(value):
+def assemble_imm8(value: str) -> str:
+    """
+    Assembly an immediate string into a 8-bit binary string.
+    :param value: the immediate value to assemble
+    :return: the immediate value as an 8-bit binary string
+    """
     value = int(value[1:], 0)
     if value < 0 or value > 255:
         raise ValueError(f"Invalid imm8 {value}")
     return f"{value:08b}"
 
 
-def assemble_imm7_offset(value):
+def assemble_imm7_offset(value: str) -> str:
+    """
+    Assembly an immediate string into a 7-bit binary string. The immediate is divided
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 7-bit binary string
+    """
     value = int(value[1:], 0)
     if value < 0 or value > 127:
         raise ValueError(f"Invalid imm7 {value}")
@@ -20,7 +35,12 @@ def assemble_imm7_offset(value):
     return f"{value:07b}"
 
 
-def assemble_imm8_offset(value):
+def assemble_imm8_offset(value: str) -> str:
+    """
+    Assembly an immediate string into a 8-bit binary string. The immediate is divided by 4
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 8-bit binary string
+    """
     value = int(value[1:], 0)
     if value < 0 or value > 255:
         raise ValueError(f"Invalid imm8 {value}")
@@ -28,14 +48,24 @@ def assemble_imm8_offset(value):
     return f"{value:08b}"
 
 
-def assemble_imm5(value):
+def assemble_imm5(value: str) -> str:
+    """
+    Assembly an immediate string into a 5-bit binary string.
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 5-bit binary string
+    """
     value = int(value[1:], 0)
     if value < 0 or value > 31:
         raise ValueError(f"Invalid imm5 {value}")
     return f"{value:05b}"
 
 
-def assemble_imm3(value):
+def assemble_imm3(value: str) -> str:
+    """
+    Assemble an immediate string into a 3-bit binary string.
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 3-bit binary string
+    """
     value = int(value[1:], 0)
     if value < 0 or value > 7:
         raise ValueError(f"Invalid imm3 {value}")
@@ -43,14 +73,34 @@ def assemble_imm3(value):
 
 
 def signed_imm8(value: int) -> str:
+    """
+    Assemble a signed immediate value into a 8-bit binary string.
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 8-bit binary string
+    """
     return signed_immn(value, 8)
 
 
 def signed_imm11(value: int) -> str:
+    """
+    Assemble a signed immediate value into a 11-bit binary string.
+    :param value: the immediate value to assemble
+    :return: the immediate value as a 11-bit binary string
+    """
     return signed_immn(value, 11)
 
 
 def signed_immn(value: int, n: int) -> str:
+    """
+    Return the 2's complement of a signed integer as a binary string.
+    :param value: the number to convert
+    :param n: the number of bits to use
+    :return: the 2's complement of the number as a binary string
+    """
     if value < 0:
         value = (1 << n) + value
     return f"{value:0{n}b}"
+
+
+def to_hex(binary: str) -> str:
+    return f"{int(binary, 2):04x}"
